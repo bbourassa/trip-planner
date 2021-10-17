264e5f0e1997c3bbb7c87e075bd1eacb2bad9d9a
bbourassa <bbourassa@umass.edu>
2021-10-17 13:29:52 -0400

    fixing deployment

--- node-rest-api/routes/trip.routes.js
+++ node-rest-api/routes/trip.routes.js
@@ -4,7 +4,7 @@ const app = express();
 const bookRoute = express.Router();
 let TripData = require('../model/Trip');
 
-bookRoute.route('/api/create').get((req, res) => {
+bookRoute.route('/create').get((req, res) => {
     res.json({'test': 'this is testdata'})
 });
 
