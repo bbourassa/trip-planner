d5eb192d06ebbdd7cd39d4540863a083ab55f7f7
bbourassa <bbourassa@umass.edu>
2021-11-26 12:17:42 -0500

    fixing reload

--- node-rest-api/index.js
+++ node-rest-api/index.js
@@ -72,7 +72,7 @@ if(process.env.NODE_ENV === 'production'){
     const path  =  require('path');
     app.use(express.static("client/build"));
     app.get('/*',(req,res)=>{
-        res.sendFile(path.resolve(__dirname,'./client/build/index.html'))
+        res.sendFile(path.resolve(__dirname,'dist/trip-planner/index.html'))
     })
 }
 
